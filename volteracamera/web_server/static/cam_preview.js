386cb20fcc44d284701f420cbf527544ced4a553
= <=>
2018-09-18 08:49:50 -0400

    updated javascript for smooth image transition.

--- volteracamera/web_server/static/cam_preview.js
+++ volteracamera/web_server/static/cam_preview.js
@@ -5,13 +5,8 @@ fetch(url, {cache: "no-store"}).then(function(response) {
   if(response.ok) {
     response.blob().then( function(blob) {
       var objectURL = URL.createObjectURL(blob);
-      var old_img = document.getElementById('preview_image');
-      var new_img = new Image();
-      new_img.id = old_img.id;
-      new_img.src = objectURL;
-      old_img.parentNode.insertBefore(new_img,old_img);
-      old_img.parentNode.removeChild(old_img);
-      console.log('Recieved ' + objectURL);
+      var img = document.getElementById('preview_image');
+      img.src = objectURL;
       setTimeout (reload_image, 500);
     });
   } else {
