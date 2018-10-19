822bfbb5f01516a5be5697d2ceb835a6d53e4ef8
= <=>
2018-10-18 21:25:26 -0400

    Added line intersection class to plane. Almost finished testing.

--- tests/plane_test.py
+++ tests/plane_test.py
@@ -4,6 +4,7 @@ Tests for the analysis plane class.
 import pytest
 import numpy as np
 from volteracamera.analysis.plane import Plane
+from volteracamera.analysis.line import Line
 
 def test_default_initializer():
     """
@@ -37,4 +38,35 @@ def test_plane_initialization_bad_normal():
     """
     with pytest.raises(ZeroDivisionError):
         Plane(normal = [0, 0, 0]) 
+
+def test_line_from_two_planes_working():
+    """
+    Test the generation of a line from two planes.
+    """
+    assert False
+
+def test_line_from_two_planes_parallel():
+    """
+    Test the fault case for two planes not intersecting / parallel
+    """
+    plane1 = Plane()
+    plane2 = Plane()
+    with pytest.raises(RuntimeError):
+        plane1.intersection_line(plane2)
+
+    plane1 = Plane(point_on_plane = [1, 1, 50], normal =[1, 0, 1] )
+    plane2 = Plane(point_on_plane = [50, 2, -5], normal = [-1, 0, -1])
+    with pytest.raises(RuntimeError):
+        plane1.intersection_line(plane2)
+
+def test_line_from_two_planes_invalid_input():
+    """
+    Test the fault case for the intersection of two planes
+    """
+    plane = Plane()
+    with pytest.raises(TypeError):
+        plane.intersection_line("not a plane")
+
+
+
 
--- volteracamera/analysis/plane.py
+++ volteracamera/analysis/plane.py
@@ -2,7 +2,7 @@
 This class is used to represent a plane given a point on the plane and a normal.
 """
 import numpy as np
-
+import math
 
 class Plane (object):
     """
@@ -27,13 +27,119 @@ class Plane (object):
         self.normal = np.asarray(normal)
         self._normalize()
     
-    def _normalize(self):
+    def _normalize(self)->None:
         """
         Normalize the plane.
         """
         denominator = np.sqrt(np.dot(self.normal, self.normal))
-        if (denominator < 0.0000001):
+        if math.isclose(denominator, 0.0):
             raise ZeroDivisionError("Normal is too close to 0.")
-        self.normal /= denominator
+        self.normal = self.normal / denominator
+
+    def intersection_line(self, other):
+        """
+        Find the intersection of two planes and return the line of intersection.
+
+        Raises Runtime Error if the planes are parallel.
+        Raises TypeError if input other is not a plane.
+
+        To get the line direction, first take the cross product of the two normals:
+
+        direction = n1 x n2
+
+        To find a point on a line, we have to solve 
+
+        a1*x + b1*y + c1*z + d1 = 0
+        a2*x + b2*y + c2*z + d2 = 0
+
+        In this program, planes are stored in point normal form, to get to the 
+        algebraic for use:
+
+        n = normal = (a, b, c)
+        r = point on plane = (x, y, z)
+        p = point in plane = (px, py, pz)
+        n . (r - p) = 0
+        a * (x - px) + b * (y - py) + c * (z - pz) = 0
+
+        a*x + b*y + c*z - n . p = 0
+        d = -n . p
+        
+        Where p = (x, y, z) is a point on the line. A third equation if needed 
+        to solve for x, y, z. For our purpose here, we will choose the point where z=0. 
+        In practice, the actual code will have two cases the solution where z=0 
+        and the solution where x = 0 to handle the case where the line lies in or 
+        parallel to the plane.
+
+        We now have a linear equation, solve using Cremer's rule 
+
+        dA = det(A) = a1*b2-b1*a2
+
+        x = (-d1*b2+b1*d2)/dA
+        y = (-a1*d2+d1*a2)/dA
+        z = 0 
+
+        or 
+
+        dA = det(A) = b1*c2 - b2*c1
+        x = 0
+        y = (-d1*c2+d2*c1)/dA
+        z = (-b1*d2+d1*b2)/dA
+
+        For the purpose of laser scanning, these two cases are sufficient, any 
+        line we produce will cross either the z or x axis (lasers won't run 
+        vertical on the sensor)
+        """
+        if not isinstance (other, Plane):
+            raise TypeError("Only plane object can be intersected with another plane.")
+
+        direction = np.cross(self.normal, other.normal)
+        dnorm = np.sqrt(np.dot(direction, direction))
+        if math.isclose (dnorm, 0.0):
+            raise RuntimeError("Two planes parallel, no line of intersection")
+
+        a1, b1, c1 = self.normal
+        d1 = - np.dot (self.normal, self.point)
+        a2, b2, c2 = other.normal
+        d2 = - np.dot (other.normal, other.point)
+
+        zdirn = np.asarray([0., 0., 1.])
+    
+        dA_z_soln = a1*b2 - b1*a2    
+        if not math.isclose(0, dA_z_soln):
+            # z=0 solution is valid
+            x = (-d1*b2+b1*d2)/dA_z_soln
+            y = (-a1*d2+d1*a2)/dA_z_soln
+            z = 0
+            return Line(point=np.asarray([x, y, z]), direction=direction)
+        
+        dA_x_soln = b1*c2 - b2*c1   
+        if not math.isclose(0, dA_x_soln):
+            # x=0 solution is valid
+            x = 0
+            y = (-d1*c2 + d2*c1)/dA_x_soln
+            z = (-b1*d2 + d1*b2)/dA_x_soln
+            return Line(point=np.asarray([x, y, z]), direction=direction)
+
+        dA_y_soln = a1*c2 - a2*c1
+        x = (-d1*c2 + d2*c1)/dA_y_soln 
+        y = 0
+        z = (-a1*d2 + a2*d1)/dA_y_soln
+        return Line(point=np.asarray([x, y, z]), direction=direction)
 
 
+    def intersection_point(self, other):
+        """
+        Find the intersection point of the given line with the plane.
+    
+        Raises Runtime Error if the line and plane are perpendicular (no solution)
+        Raises TypeError if other is not a line type.
+        """
+        if not isinstance (other, Line):
+            raise TypeError("Only a Line can intersect a plane at a point.")
+        
+        norm = np.dot (self.normal, other.direction)
+
+        if math.isclose(norm, 0.0):
+            raise RuntimeError ("Line and plane are perpendicular, no intersection.")
+
+        
