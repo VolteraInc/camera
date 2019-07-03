01f6c83abf8f688807fe00b13a3863e074902b64
= <=>
2019-07-02 20:25:24 -0400

    Python module now building as part of the pip install process.

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-from setuptools import setup
+from skbuild import setup
 
 setup(
     name='VolteraCamera',
     version='0.1dev',
-    packages=['volteracamera',],
+    packages=['volteracamera', ],
     license='Voltera Inc.',
     long_description=open('README.md').read(),
-    entry_points = {
+    entry_points={
         'console_scripts': ['FindLaser=volteracamera.analysis.laser_line_finder:preview_image'],
-    }    
+    }
 )
-
